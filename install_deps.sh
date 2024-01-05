26990e627ca4466c17058c3c68748ed0627fcaea
Monika <monika.ivanova.encheva@gmail.com>
2024-01-06 00:50:39 +0200

    Updating dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get -y install gcc g++ make cmake libuv1-dev git
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
-# git checkout e94b6e1
+git checkout e94b6e1
 mkdir build
 cd build
 cmake ..
